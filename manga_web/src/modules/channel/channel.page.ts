b1211f3d1c42f911f5fbc7872b693ca0407647ea
dongha <vandong9@gmail.com>
2022-11-29 01:17:41 +0700

    update

--- manga_web/src/modules/channel/channel.page.ts
+++ manga_web/src/modules/channel/channel.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IChannelModel } from 'src/models/channel';
 import { VideoService } from 'src/services/video.service';
 
 @Component({
@@ -11,6 +12,29 @@ export class ChannelPage implements OnInit {
   channelID?: String;
   toggle: Boolean = false;
 
+  channelModel: IChannelModel = {
+    id: '123456',
+    name: 'KKBOX 華語單曲',
+    avatar:
+      'https://yt3.ggpht.com/8gDo56CtvBhnZMMDY7LtnOHlEDnGLyTLiHPcwiRK_4UAn37U-ZP-C4Zw0mB6ij8vKIXTYlsGjQ=s88-c-k-c0x00ffffff-no-rj',
+    subcriberscount: 0,
+    subcribers: '12k',
+  };
+
+  menuItems: IChanneMenuItem[] = [
+    { id: 'home', title: 'Home' },
+    { id: 'videos', title: 'Videos' },
+    { id: 'playlist', title: 'Playlist' },
+    { id: 'community', title: 'community' },
+    { id: 'channels', title: 'Channels' },
+  ];
+
+  currentSelectedMenuItemID: String = 'home';
+
+  onSelectMenuItem(id: String) {
+    this.currentSelectedMenuItemID = id;
+  }
+
   constructor(
     private _route: ActivatedRoute,
     private videoService: VideoService
@@ -21,7 +45,11 @@ export class ChannelPage implements OnInit {
   }
 
   listenToggleHambegerEmit(event: any) {
-    console.log(event);
     this.toggle = !this.toggle;
   }
+}
+
+interface IChanneMenuItem {
+  id: String;
+  title: String;
 }
