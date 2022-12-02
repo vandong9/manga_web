c0c646c2162b468e9b3e8151d64ec7b855890b23
dongha <vandong9@gmail.com>
2022-12-03 02:45:34 +0700

    update

--- manga_web/src/components/video-horizon-list/video-horizon-list.component.ts
+++ manga_web/src/components/video-horizon-list/video-horizon-list.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { IVideoLinkItem } from '../../models/video-link-item';
 import { VideoService } from 'src/services/video.service';
 
 @Component({
   selector: 'video-horizon-list',
-  templateUrl:'video-horizon-list.component.html'
+  templateUrl: 'video-horizon-list.component.html',
 })
-export class VideoHorizonListComponent {
-  @Input() isShowChannel: Boolean = true
-  @Input() numOfColumn: number = 4
+export class VideoHorizonListComponent implements AfterViewInit {
+  @Input() isShowChannel: Boolean = true;
+  @Input() numOfColumn: number = 4;
   @Input() videoLinks: IVideoLinkItem[] = [];
-  
+
+  @ViewChild('ItemContainer') itemContainer?: ElementRef;
+
+  getValue() {
+    console.log(this.itemContainer);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.itemContainer?.nativeElement.innerHTML);
+  }
+  onClickLeft() {
+    if (this.itemContainer != null) {
+      this.itemContainer.nativeElement.scrollLeft -= 200;
+    }
+  }
+
+  onClickRight() {
+    if (this.itemContainer != null) {
+      this.itemContainer.nativeElement.scrollLeft += 200;
+    }
+  }
 }
